a957734287bcdb18131a1a52083c604c75f00ec8
alan lima <alan.silva1f@gmail.com>
2022-02-09 21:31:39 -0300

    Melhorando o projeto

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 let menuAtivo = document.querySelectorAll("[data-menu]");
 
+//Menu
+let menuHambuguer = document.querySelector(".menuHambuguer");
+let removeNavBar =  document.getElementById("removebars")
+let navMenu = document.querySelector(".navbar");
+
+menuHambuguer.addEventListener("click", ()=>{
+    navMenu.classList.add("active");
+});
+
+removeNavBar.addEventListener("click", () =>{
+    navMenu.classList.remove("active");
+});
+
 menuAtivo.forEach((menu)=>{
     menu.addEventListener("click",function(){
         for(i = 0; i < menuAtivo.length; i++){
@@ -8,4 +21,3 @@ menuAtivo.forEach((menu)=>{
         menu.classList.add("ativo");
     });
 });
-
